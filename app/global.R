e2daccf4025e9484ef44dc4890bfa97c53aac839
Hankun Shi <54440572+HankunShi@users.noreply.github.com>
2020-10-12 17:03:06 -0400

    add: import data for dashboard v1

--- app/global.R
+++ app/global.R
@@ -64,9 +64,29 @@ if (!require("viridis")) {
 ###############################Data Preparation#######################
 #Data Sources
 
-#Download the park crowds data in this link and aftering cleaning it, put it to output file
+#Download the park crowds data in this link and after cleaning it, put it to output file
 # https://data.cityofnewyork.us/dataset/Social-Distancing-Parks-Crowds-Data/gyrw-gvqc
 
 setwd('..')
 output_shapefile_filepath <- "./output/clean_parks_data.csv"
 park_data<-read.csv(output_shapefile_filepath)
+
+
+############
+
+# read ZIP_CODE_040114.shp
+path_zip = "./data/"
+file_zip = "ZIP_CODE_040114.shp"
+zipcode_geo <- sf::st_read(paste0(path_zip, file_zip)) %>%
+  sf::st_transform('+proj=longlat +datum=WGS84')
+
+# read park data
+park_join = read_csv(getURL("./output/park_join_4map.csv"),
+                         col_types = cols())
+
+park_join.action_taken = c("Approached the crowd; they ignored the employee",
+                           "Approached the crowd; they complied with instructions",
+                           "Did not approach the crowd; the crowd remains")
+
+# read covid cases data
+case_df = read_csv(getURL("./output/daily_cases_by_boro.csv"))
