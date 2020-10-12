60ba4d1dde185be37b23d469293d058da70f0cdd
rl2886-ray <62366260+rl2886-ray@users.noreply.github.com>
2020-10-12 12:56:54 -0400

    update global and server
    update global and server

--- app/global.R
+++ app/global.R
@@ -59,100 +59,14 @@ if (!require("viridis")) {
 }
 #--------------------------------------------------------------------
 ###############################Define Functions#######################
-data_cooker <- function(df){
-  #input dataframe and change the Country/Region column into standard format
-  df$Country.Region <- as.character(df$Country.Region)
-  df$Country.Region[df$Country.Region == "Congo (Kinshasa)"] <- "Dem. Rep. Congo"
-  df$Country.Region[df$Country.Region == "Congo (Brazzaville)"] <- "Congo"
-  df$Country.Region[df$Country.Region == "Central African Republic"] <- "Central African Rep."
-  df$Country.Region[df$Country.Region == "Equatorial Guinea"] <- "Eq. Guinea"
-  df$Country.Region[df$Country.Region == "Western Sahara"]<-"W. Sahara"
-  df$Country.Region[df$Country.Region == "Eswatini"] <- "eSwatini"
-  df$Country.Region[df$Country.Region == "Taiwan*"] <- "Taiwan"
-  df$Country.Region[df$Country.Region== "Cote d'Ivoire"] <-"Côte d'Ivoire"
-  df$Country.Region[df$Country.Region == "Korea, South"] <- "South Korea"
-  df$Country.Region[df$Country.Region == "Bosnia and Herzegovina"] <- "Bosnia and Herz."
-  df$Country.Region[df$Country.Region == "US"] <- "United States of America"
-  df$Country.Region[df$Country.Region == "Burma"]<-"Myanmar"
-  df$Country.Region[df$Country.Region == "Holy See"]<-"Vatican"
-  df$Country.Region[df$Country.Region=="South Sudan"]<-"S. Sudan"
-  return(df)
-}
 
-
-data_transformer <- function(df) {
-  #################################################################
-  ##Given dataframe tranform the dataframe into aggregate level with
-  ##rownames equal to countries name, and colnames equals date
-  #################################################################
-  #clean the country/regionnames
-  df <- data_cooker(df)
-  #columns that don't need 
-  not_select_cols <- c("Province.State","Lat","Long")
-  #aggregate the province into country level
-  aggre_df <- df %>% group_by(Country.Region) %>% 
-    select(-one_of(not_select_cols)) %>% summarise_all(sum)
-  #assign the country name into row names 
-  aggre_df <- aggre_df %>% remove_rownames %>% 
-    tibble::column_to_rownames(var="Country.Region")
-  #change the colume name into date format
-  date_name <- colnames(aggre_df)
-  #change e.g: "x1.22.20" -> "2020-01-22"
-  date_choices <- as.Date(date_name,format = 'X%m.%d.%y')
-  #assign column nam
-  colnames(aggre_df) <- date_choices
-  return(aggre_df)
-}
 #--------------------------------------------------------------------
 ###############################Data Preparation#######################
 #Data Sources
-"Dong E, Du H, Gardner L. An interactive web-based dashboard to track COVID-19 in real time. 
-Lancet Inf Dis. 20(5):533-534. doi: 10.1016/S1473-3099(20)30120-1"
-#get the daily global cases data from API
-Cases_URL <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-global_cases <- read.csv(text = Cases_URL)
 
-#get the daily global deaths data from API
-Death_URL <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
-global_death <- read.csv(text = Death_URL)
+#Download the park crowds data in this link and aftering cleaning it, put it to output file
+# https://data.cityofnewyork.us/dataset/Social-Distancing-Parks-Crowds-Data/gyrw-gvqc
 
-
-#get aggregate cases 
-aggre_cases <- as.data.frame(data_transformer(global_cases))
-#get aggregate death
-aggre_death <- as.data.frame(data_transformer(global_death))
-#define date_choices 
-date_choices <- as.Date(colnames(aggre_cases),format = '%Y-%m-%d')
-#define country_names
-country_names_choices <- rownames(aggre_cases)
-
-#Download the spatial polygons dataframe in this link
-# https://www.naturalearthdata.com/downloads/50m-cultural-vectors/50m-admin-0-countries-2/
-
-output_shapefile_filepath <- "./output/countries_shapeFile.RData"
-
-#if already has countries_shapeFile.RData under output folder, no need to process it again
-#otherwise, read files from data folder to create countries_shapeFile.RData under output folder
-if(file.exists(output_shapefile_filepath)){
-  load(output_shapefile_filepath)
-}else{
-  countries <- readOGR(dsn ="../data/ne_50m_admin_0_countries",
-                       layer = "ne_50m_admin_0_countries",
-                       encoding = "utf-8",use_iconv = T,
-                       verbose = FALSE)
-  save(countries, file=output_shapefile_filepath)
-}
-
-
-#make a copy of aggre_cases dataframe
-aggre_cases_copy <- as.data.frame(aggre_cases)
-aggre_cases_copy$country_names <- as.character(rownames(aggre_cases_copy))
-
-#make a copy of aggre_death dataframe
-aggre_death_copy <- as.data.frame(aggre_death)
-aggre_death_copy$country_names <- as.character(rownames(aggre_death_copy))
-
-binning<- function(x) {10^(ceiling(log10(x)))}
-
-#use save.image() at any time to save all environment data into an .RData file
-save.image(file='./output/covid-19.RData')
+setwd('..')
+output_shapefile_filepath <- "./output/clean_parks_data.csv"
+park_data<-read.csv(output_shapefile_filepath)
--- app/ui.R
+++ app/ui.R
@@ -3,95 +3,58 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-#ag
+#
 #    http://shiny.rstudio.com/
 #
-# Define UI for application that draws a histogram
-library(viridis)
-library(dplyr)
-library(tibble)
-library(tidyverse)
-library(shinythemes)
-library(sf)
-library(RCurl)
-library(tmap)
-library(rgdal)
-library(leaflet)
+
 library(shiny)
-library(shinythemes)
-library(plotly)
-library(ggplot2)
-#load('./output/covid-19.RData')
-shinyUI(navbarPage(title = 'COVID-19',
-                   fluid = TRUE,
-                   collapsible = TRUE,
-                   #Select whichever theme works for the app 
-                   theme = shinytheme("journal"),
-                   #--------------------------
-                   #tab panel 1 - Home
-                   tabPanel("Home",icon = icon("home"),
-                            fluidPage(
-                              fluidRow(
-                                column(12,
-                                       h1("Global Cases overview across time"),
-                                       fluidRow(
-                                         #select the date until now
-                                         column(6,
-                                                sliderInput('date','Date Unitl:',
-                                                            #first day of data recording
-                                                            min = as.Date(date_choices[1]),
-                                                            #present day of data recording
-                                                            max = as.Date(tail(date_choices,1)),
-                                                            value = as.Date(date_choices[1]),
-                                                            timeFormat = "%Y-%m-%d",
-                                                            animate = TRUE, step = 5),
-                                                fluidRow(
-                                                  #select the country we want to see the trend
-                                                  column(6, 
-                                                         selectInput('country','Which Country?',
-                                                                     choices = country_names_choices,
-                                                                     selected = 'United States of America')),
-                                                  #select whether want case number in log-scale or not
-                                                  column(6,
-                                                         radioButtons("log_scale", "In Log Scale:",
-                                                                      choices = c(TRUE,FALSE),
-                                                                      selected = FALSE))
-                                                          )
-                                                ),
-                                         #render plotly output
-                                         column(width = 6,
-                                                plotlyOutput('case_overtime'))
-                                              )
-                                        )
-                                      )
-                                    )
-                                ),
-                   #--------------------------
-                   #tab panel 2 - Map
-                   tabPanel("Maps",icon = icon("map-marker-alt"),div(class = 'outer',
-                            leafletOutput("map", width = "100%", height = "1200"),
-                            absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                          top = 300, left = 20, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                          selectInput('choices','Which data to visualize:',
-                                                      choices = c('Cases','Death'),
-                                                      selected = c('Cases')),
-                                          sliderInput('date_map','Input Date:',
-                                                      #first day of data recording
-                                                      min = as.Date(date_choices[1]),
-                                                      #present day of data recording
-                                                      max = as.Date(tail(date_choices,1)),
-                                                      value = as.Date('2020-04-01','%Y-%m-%d'),
-                                                      timeFormat = "%Y-%m-%d",
-                                                      animate = TRUE, step = 5),
-                                          style = "opacity: 0.80"))),
-                   # ----------------------------------
-                   #tab panel 3 - Source
-                   tabPanel("Data Source",icon = icon("cloud-download"),
-                            HTML(
-                              "<h2> Data Source : </h2>
-                              <h4> <p><li><a href='https://coronavirus.jhu.edu/map.html'>Coronavirus COVID-19 Global Cases map Johns Hopkins University</a></li></h4>
-                              <h4><li>COVID-19 Cases : <a href='https://github.com/CSSEGISandData/COVID-19' target='_blank'>Github Johns Hopkins University</a></li></h4>
-                              <h4><li>Spatial Polygons : <a href='https://www.naturalearthdata.com/downloads/' target='_blank'> Natural Earth</a></li></h4>"
-                            ))
-                   
-))
+library(shinydashboard)
+
+dashboardPage(
+    skin = "green",
+    title = "RadaR",
+    dashboardHeader(title = "Social Distancing & Covid 19",
+                    titleWidth = 300),
+    dashboardSidebar( 
+        sidebarMenu(
+        menuItem("Home&Introduction", tabName = "home", icon = icon("dashboard")),
+        menuItem("Covid_Dashboard or Map", tabName = "dashboard", icon = icon("map")),
+        menuItem("Social_Distance", tabName = "sd", icon = icon("people-arrows")),
+        menuItem("Data_Source", tabName = "source", icon = icon("database"))
+    )
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            #home
+            tabItem(tabName = "home",
+                    fluidPage(
+                        fluidRow(
+                            box(width = 15, title = "Introduction", status = "success",
+                                solidHeader = TRUE, h3("Social_Distancing & Covid"),
+                                h4("By Qinzhe Hu, Depeng Kong, Rui Liang, Yotam Segal, Hankun Shi"),
+                               )),
+                        fluidRow(box(width = 15, title = "User Guide", status = "success",
+                                     solidHeader = TRUE, h3("What Does This App Do?"),
+                                     tags$div(tags$ul(
+                                         tags$li("Covid_Dashboard&Map: This part contains "),
+                                         tags$li("Social_Distance: This part is "),
+                                         tags$li("Data_Source: ")
+                                     ))))
+                    )),
+            # Second tab content
+            tabItem(tabName = "dashboard",
+                    h2("covid tab content")
+            ),
+            # Third tab content
+            tabItem(tabName = "sd",
+                    h2("social distance tab content")
+            ),
+            # Fourth tab content
+            tabItem(tabName = "source",
+                    h2("Data source tab content")
+            )
+        )
+    )
+)
+
