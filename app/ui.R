628552d24171baa03d73bfaddb7b0f1e07d78ccf
Hankun Shi <54440572+HankunShi@users.noreply.github.com>
2020-10-12 17:01:52 -0400

    add dashboard v1

--- app/ui.R
+++ app/ui.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(shinydashboard)
+library(lubridate)
 
 dashboardPage(
     skin = "green",
@@ -44,7 +45,25 @@ dashboardPage(
                     )),
             # Second tab content
             tabItem(tabName = "dashboard",
-                    h2("covid tab content")
+                    
+                    ui = fluidPage(
+                        h2("heatmap"),
+                        leafletOutput("map_park_covid", width = "100%", height = "1200"),
+                        absolutePanel(id = "control", class = "panel panel-default", fixed = F, draggable = TRUE,
+                                      top = 100, left = 20, right = "auto", bottom = "auto", width = 300, height = "auto",
+                                      
+                                      h4("Which covid cases to visualize:"),
+                                      checkboxInput("if_covid_boro_map", "historical cases at borough level", value = T),
+                                      # checkboxInput("if_covid_zip_map", "today's cases at zipcode level", value = TRUE),
+                                      sliderInput('date_map','Input Date:', value = lubridate::ymd('2020-05-01'), 
+                                                  min = lubridate::ymd('2020-03-25'), max = lubridate::ymd(('2020-06-30')), 
+                                                  timeFormat = "%Y-%m-%d",animate = TRUE, step = 1),
+                                      radioButtons(inputId = "cum_choice_new_map","cumulative / daily new",
+                                                   choices = list("cumulative","new"),selected = "cumulative"),
+                                      sliderInput(inputId = "covid_fillOpacity_map", label = "fill Opacity",
+                                                  value=0.5,min=0,max=1,step=0.01,ticks = F,dragRange = F),
+                                      style = "opacity: 0.90"
+                        ))
             ),
             # Third tab content
             tabItem(tabName = "sd",
